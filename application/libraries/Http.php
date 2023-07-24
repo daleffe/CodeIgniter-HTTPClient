d5ef32854077dd7ed193373b274fb570f12f0dd9
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2023-07-24 12:54:18 -0300

    Added PUT/PATCH/DELETE methods

--- application/libraries/Http.php
+++ application/libraries/Http.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace Haruncpi\Http;
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Http
 {
@@ -13,6 +11,21 @@ class Http
      * POST constant value.
      */
     const TYPE_POST = 'POST';
+
+    /**
+     * PUT constant value.
+     */
+    const TYPE_PUT = 'PUT';
+
+    /**
+     * PATCH constant value.
+     */
+    const TYPE_PATCH = 'PATCH';
+
+    /**
+     * DELETE constant value.
+     */
+    const TYPE_DELETE = 'DELETE';
 
     /**
      * Hold response raw body.
@@ -36,57 +49,75 @@ class Http
     private $headers;
 
     /**
-     * Common curl request method for GET and POST request
+     * Default cURL options
      *
-     * @param string    $type          HTTP verb GET, POST.
+     * @var array
+     */
+     private $options;
+
+    /**
+     * Constructor
+     *
+     * @var array
+     */
+    public function __construct($curlOptions = []){
+        $default = [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYPEER => false
+        ];
+
+        $this->options = array_replace($default, $curlOptions);
+
+        // Must required for get headers and body
+        $this->options[CURLOPT_HEADER] = 1;
+	}
+
+    /**
+     * Common curl request method for request
+     *
+     * @param string    $type          HTTP verb: GET, POST, PUT, PATCH or DELETE.
      * @param string    $url           Request URL.
      * @param array     $data          Request data.
      * @param array     $headers       Headers data.
-     * @param array     $curlOptions   curl options.
+     * @param array     $curlOptions   cURL options.
      * @return void
      */
-    private function curlRequest($type, $url, $data = [], $headers = [], $curlOptions = [])
+    private function request($type, $url, $data = [], $headers = [], $curlOptions = [])
     {
         $curl = curl_init();
-        $type = strtoupper($type);
 
         if (self::TYPE_GET === $type && count($data)) {
             $url = $url . '?' . http_build_query($data);
         }
 
-        $default = array(
-            CURLOPT_URL             => $url,
-            CURLOPT_RETURNTRANSFER  => true,
-            CURLOPT_ENCODING        => "",
-            CURLOPT_MAXREDIRS       => 10,
-            CURLOPT_TIMEOUT         => 30,
-            CURLOPT_CUSTOMREQUEST   => $type,
-            CURLOPT_HTTPHEADER      => $headers,
-        );
+        $options = array_replace($this->options, $curlOptions);
 
-        $finalCurlOptions = array_replace($default, $curlOptions);
-
-        // Must required for get headers and body
-        $finalCurlOptions[CURLOPT_HEADER] = 1;
-
-        if (self::TYPE_POST === $type && count($data)) {
-            $finalCurlOptions[CURLOPT_POSTFIELDS] = json_encode($data);
+        if (in_array($type,[self::TYPE_POST,self::TYPE_PUT,self::TYPE_PATCH]) && count($data)) {
+            $options[CURLOPT_POST] = 1;
+            $options[CURLOPT_POSTFIELDS] = json_encode($data);
         }
 
-        curl_setopt_array($curl, $finalCurlOptions);
+        $options[CURLOPT_URL] = $url;
+        $options[CURLOPT_CUSTOMREQUEST] = strtoupper($type);
+        $options[CURLOPT_HTTPHEADER] = $headers;
+
+        curl_setopt_array($curl, $options);
 
         $response = curl_exec($curl);
-        $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-        $resonseHeaders = substr($response, 0, $header_size);
-        $body = substr($response, $header_size);
+        $headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+
+        $this->statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $this->headers = substr($response, 0, $headerSize);
+        $this->body = substr($response, $headerSize);
 
         curl_close($curl);
 
-        $this->body = $body;
-        $this->statusCode = $statusCode;
-        $this->headers = $resonseHeaders;
+        return $this;
     }
 
     /**
@@ -98,11 +129,9 @@ class Http
      * @param   array     $curlOptions
      * @return  object
      */
-    public static function get($url, $data = [], $headers = [], $curlOptions = [])
+    public function get($url, $data = [], $headers = [], $curlOptions = [])
     {
-        $obj = new self;
-        $obj->curlRequest(self::TYPE_GET, $url, $data, $headers, $curlOptions);
-        return $obj;
+        return $this->request(self::TYPE_GET, $url, $data, $headers, $curlOptions);
     }
 
     /**
@@ -114,11 +143,50 @@ class Http
      * @param   array   $curlOptions
      * @return  object
      */
-    public static function post($url, $data = [], $headers = [], $curlOptions = [])
+    public function post($url, $data = [], $headers = [], $curlOptions = [])
     {
-        $obj = new self;
-        $obj->curlRequest(self::TYPE_POST, $url, $data, $headers, $curlOptions);
-        return $obj;
+        return $this->request(self::TYPE_POST, $url, $data, $headers, $curlOptions);
+    }
+
+    /**
+     * PUT request
+     *
+     * @param   string  $url
+     * @param   array   $data
+     * @param   array   $headers
+     * @param   array   $curlOptions
+     * @return  object
+     */
+    public function put($url, $data = [], $headers = [], $curlOptions = [])
+    {
+        return $this->request(self::TYPE_PUT, $url, $data, $headers, $curlOptions);
+    }
+
+    /**
+     * PATCH request
+     *
+     * @param   string  $url
+     * @param   array   $data
+     * @param   array   $headers
+     * @param   array   $curlOptions
+     * @return  object
+     */
+    public function patch($url, $data = [], $headers = [], $curlOptions = [])
+    {
+        return $this->request(self::TYPE_PATCH, $url, $data, $headers, $curlOptions);
+    }
+
+    /**
+     * DELETE request
+     *
+     * @param   string  $url
+     * @param   array   $headers
+     * @param   array   $curlOptions
+     * @return  object
+     */
+    public function delete($url, $headers = [], $curlOptions = [])
+    {
+        return $this->request(self::TYPE_DELETE, $url, [], $headers, $curlOptions);
     }
 
     /**
@@ -140,8 +208,8 @@ class Http
     {
         $headers = array();
         $arrRequests = explode("\r\n\r\n", $this->headers);
-        for ($index = 0; $index < count($arrRequests) - 1; $index++) {
 
+        for ($index = 0; $index < count($arrRequests) - 1; $index++) {
             foreach (explode("\r\n", $arrRequests[$index]) as $i => $line) {
                 if ($i === 0)
                     $headers[$index]['http_code'] = $line;
@@ -164,11 +232,8 @@ class Http
     public function getHeader($key)
     {
         $headers = $this->getHeaders();
-        if (is_array($headers)) {
-            return $headers[$key];
-        } else {
-            return null;
-        }
+
+        return is_array($headers) ? (isset($headers[$key]) ? $headers[$key] : FALSE) : NULL;
     }
 
     /**
@@ -188,7 +253,7 @@ class Http
      */
     public function getJson()
     {
-        return json_decode($this->body, true);
+        return json_decode($this->getBody(), true);
     }
 
     /**
@@ -198,6 +263,8 @@ class Http
      */
     public function getObject()
     {
-        return json_decode($this->body);
+        return json_decode($this->getBody());
     }
 }
+
+/* End of file HttpClient.php */
